29b24725ac79c7baaf5cab89dc2af48649286675
Rodrigo Miranda <rodrigo.mira08@gmail.com>
2023-12-25 20:28:21 -0300

    chore: eslint/prettier and tailwind setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +10,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--sans-font)', ...defaultTheme.fontFamily.sans],
+        grifter: ['Grifter', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
