a3be5d60f6a54669dfdb33ce6807d467f97f0bc0
Rodrigo Miranda <rodrigo.mira08@gmail.com>
2024-01-14 14:23:51 -0300

    feat: add metadata and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
 import './globals.css';
 
+import { Metadata } from 'next';
 import { DM_Sans as DMSans } from 'next/font/google';
 import { useLocale } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 const DmSans = DMSans({
   variable: '--sans-font',
@@ -10,7 +12,23 @@ const DmSans = DMSans({
   weight: ['400', '500', '600', '700'],
 });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+interface PageProps {
+  params: { locale: string };
+}
+
+export async function generateMetadata({ params: { locale } }: PageProps): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'metadata' });
+
+  return {
+    title: 'Rodrigo Miranda',
+    description: t('description'),
+    openGraph: {
+      images: [t('ogImage')],
+    },
+  };
+}
+
+export default function RootLayout({ children }: PageProps & { children: ReactNode }) {
   const locale = useLocale();
 
   return (
